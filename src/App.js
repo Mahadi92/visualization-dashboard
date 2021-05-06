1214cde1343878597ddd183f2dcd493b57152ee3
Mahadi92 <mahadi.hasan.937524.mh@gmail.com>
2021-05-07 02:48:03 +0600

    Starting visualization-dashboard

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Dashboad from './components/Dashboard/Dashboad';
+
+function App() {
+  return (
+    <div>
+      <Dashboad></Dashboad>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Dashboard/Dashboad.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dashboad = () => {
+    return (
+        <div>
+            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, facilis fugit. At, suscipit. Fuga vitae, nisi voluptatem optio autem libero ipsam corrupti. Officiis voluptatem odio quaerat autem. Sequi, eius assumenda?</h1>
+        </div>
+    );
+};
+
+export default Dashboad;
